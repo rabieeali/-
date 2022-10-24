dc7d4a4e5e0445b587cf9a83767527c4f56c75b0
rabieeali <alirabiei1375@gmail.com>
2022-10-24 15:39:36 +0330

    added 4 more bands

--- src/db.js
+++ src/db.js
@@ -16,5 +16,65 @@ module.exports = [
       "Alternative metal",
       "Nu metal",
     ],
+    image:
+      "https://imageio.forbes.com/specials-images/imageserve/61bb718d9b2dda82b26dd78a/0x0.jpg?format=jpg&width=1200",
+  },
+  {
+    id: 2,
+    band: "August Burns Red",
+    members: [
+      { name: "Jake Luhrs", role: "Singer" },
+      { name: "JB Brubaker", role: "Guitarist" },
+      { name: "Brent Rambler", role: "Guitarist" },
+      { name: "Dustin Davidson", role: "Bassist" },
+      { name: "Matt Greiner", role: "Drummer" },
+    ],
+    origin: ["Lancaster", "Pennsylvania", "U.S."],
+    genres: ["Metalcore", "Melodic metalcore", "Christian metal"],
+    image:
+      "https://images.squarespace-cdn.com/content/v1/5f57fe3352a66d1bd7478dc9/1621965928204-PF0YEY4FSK3DNOV8UT5V/ABR-2021.jpeg",
+  },
+  {
+    id: 3,
+    band: "Periphery",
+    members: [
+      { name: "Spencer Sotelo", role: "Singer" },
+      { name: "Misha Mansoor", role: "Guitarist" },
+      { name: "Mark Holcomb", role: "Guitarist" },
+      { name: "Jake Bowen", role: "Guitarist" },
+      { name: "Tom Murphy", role: "Bassist" },
+      { name: "Matt Halpern", role: "Drummer" },
+    ],
+    origin: ["Washington", "D.C.", "U.S."],
+    genres: ["Progressive metal", "Djent", "Progressive metalcore"],
+    image: "https://amassing2.sakura.ne.jp/image/jacket/large/2020b/114650.jpg",
+  },
+  {
+    id: 4,
+    band: "Architects",
+    members: [
+      { name: "Sam Carter", role: "Singer" },
+      { name: "Josh Middleton", role: "Guitarist" },
+      { name: "Adam Christianson", role: "Guitarist" },
+      { name: "Alex Dean", role: "Bassist" },
+      { name: "Dan Searle", role: "Drummer" },
+    ],
+    origin: ["Brighton", "East Sussex", "England"],
+    genres: ["Metalcore", "Progressive metalcore", "Post-hardcore"],
+    image: "https://mistshelter.com/wp-content/uploads/2018/11/architects.webp",
+  },
+  {
+    id: 5,
+    band: "Jinjer",
+    members: [
+      { name: "Tatiana Shmailyuk", role: "Singer" },
+      { name: "Roman Ibramkhalilov ", role: "Guitarist" },
+      { name: "Eugene Abdukhanov", role: "Bassist" },
+      { name: "Vladislav Ulasevich", role: "Drummer" },
+    ],
+    origin: ["Donetsk", "Ukraine"],
+    genres: ["Metalcore ", "Progressive metal", "Groove metal", "Djent"],
+    image:
+      "https://static.tildacdn.com/tild3835-3363-4634-b832-323232336334/jinjer2021_1000px.jpg",
   },
 ];
